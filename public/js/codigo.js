a2ef0f79ee6cec22426bbf38910111d945b2139a
gloria <g.gloria142@gmail.com>
2017-04-17 19:30:01 -0500

    Ejemplo Vue Objetos Arreglos , enlace de datos bidireccionales, interpolación

---
+++ public/js/codigo.js
@@ -0,0 +1,16 @@
+new Vue ({
+    el: 'main',
+    data : {
+        texto : 'Hola mundo des vue2',
+        nombre: 'Mariana',
+        nota : 5,
+        peliculas :  ['Batman Vs SUperman', 'La Verdad duele', 'Spiderman'],
+        objectMovie : [
+            {'pelicula': 'Volver al futuro', anio: '2017', mes : 'Enero'},
+            {'pelicula': 'Batman', anio: '2020', mes : 'Enero'},
+            {'pelicula': 'Superman', anio: '2020', mes : 'Febrero'}
+
+            ],
+        frutas: { nombre: 'mandarina', temporada : 'verano' }
+    }
+});
